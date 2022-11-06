771a16ae27b602a50013a56126221c8a5fd86413
Johhan Urrutia <johhanurrutia@MacBook-Pro-de-Johhan.local>
2022-11-06 18:28:50 -0300

    Correccion v1.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,18 @@
-const CantidadIngresada = document.getElementById ("CantidadIngresada")
-const ColorIngresado = document.getElementById("ColorIngresado")
-const BotonCalcular = document.getElementById("BotonCalcular")
-var TotalFinal = document.getElementById("TotalFinal")
-var CantidadFinal = document.getElementById("CantidadFinal")
-var ColorFinal = document.getElementById("ColorFinal")
-var MensajeError = document.getElementById("MensajeError")
+let CantidadIngresada = document.getElementById ("CantidadIngresada")
+let ColorIngresado = document.getElementById("ColorIngresado")
+let BotonCalcular = document.getElementById("BotonCalcular")
+let TotalFinal = document.getElementById("TotalFinal")
+let CantidadFinal = document.getElementById("CantidadFinal")
+let ColorFinal = document.getElementById("ColorFinal")
+let MensajeError = document.getElementById("MensajeError")
 
 BotonCalcular.addEventListener("click", () => {
     if (parseFloat(CantidadIngresada.value) > 0){
-        var TotalFinalFormula = () => {
-            CantidadIngresada.innerHTML 
-        }
         TotalFinal.innerHTML = (parseFloat(CantidadIngresada.value) * 400000)
         CantidadFinal.innerHTML = parseFloat(CantidadIngresada.value)
         ColorFinal.style.backgroundColor = ColorIngresado.value
     }else{
         MensajeError.innerHTML = "Favor Ingresa un número mayor a 0*"
+        setTimeout(function(){ MensajeError.innerHTML = "" }, 1500);
     }
 })
